83b3a03d5ce7c585eb3b7baacf50661819551569
Brent Guistwite <brentguistwite@gmail.com>
2018-02-16 14:40:59 -0500

    nav bar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,59 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addNote, updateNoteInDb } from './../actions';
 import newNote from './../images/newNote.png';
-import hideList from './../images/hideList.png';
+import save from './../images/save.png';
 
 export function Navbar(props) {
   return(
-    <nav className="top-nav">
-      <img 
-        className="new-note-button" 
-        src={newNote} 
-        onClick={() => props.dispatch(addNote())} />
-      <input 
-        onChange={(e) => console.log(e.target.value)} 
-        type="text" 
-        placeholder="Search notes"/>
-      <label htmlFor="preview">Preview notes</label>
-      <input
-        className="orange" 
-        type="checkbox" 
-        id="preview"
-      />
-      <span>Sort by:</span>
-      <label htmlFor="recent">Recent</label>
-      <input
-        className="orange" 
-        checked="checked"
-        type="checkbox" 
-        id="recent"/>
-      <label htmlFor="alphabetic">Alphabetic</label>
-      <input
-        className="orange"
-        type="checkbox"
-        id="alphabetic" />
-      <img 
-        className="save-note-button" 
-        src={hideList} 
-        onClick={() => props.dispatch(updateNoteInDb(props.currentDraft))} />
-    </nav> 
-  )
+    <header className="header">
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <img 
+              className="new-note-button" 
+              src={newNote} 
+              onClick={() => props.dispatch(addNote())}
+            />
+          </li>
+          <li>
+            <input 
+              onChange={(e) => console.log(e.target.value)} 
+              type="text" 
+              placeholder="Search notes"/>
+          </li>
+          <li>
+            <label htmlFor="preview">Preview notes</label>
+            <input
+              type="checkbox" 
+              id="preview"
+            />
+          </li>
+          <li>
+            <span>Sort by:</span>
+            <label htmlFor="recent">Recent</label>
+            <input
+              name="sorted-by"
+              checked
+              type="radio" 
+              id="recent"/>
+            <label htmlFor="alphabetic">Alphabetic</label>
+            <input
+              name="sorted-by"
+              type="radio"
+              id="alphabetic" 
+            />
+          </li>
+          <li>
+            <img 
+              className="save-note-button" 
+              src={save} 
+              onClick={() => props.dispatch(updateNoteInDb(props.currentDraft))} 
+            />
+          </li>
+        </ul>
+      </nav> 
+    </header>
+  );
 }
 
 export default connect()(Navbar);
