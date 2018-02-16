ffcf08ca269ff61b1395d4822c0edf15175abb23
Brent Guistwite <brentguistwite@gmail.com>
2018-02-16 13:17:37 -0500

    new items on savbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addNote } from './../actions';
-import addButton from './../addButton.png';
+import { addNote, updateNoteInDb } from './../actions';
+import newNote from './../images/newNote.png';
+import hideList from './../images/hideList.png';
 
 export function Navbar(props) {
   return(
-    <nav>
-      <img className="new-note-button" src={addButton} onClick={() => props.dispatch(addNote())} />
-      <input onChange={(e) => console.log(e.target.value)} type="text" placeholder="Search notes"/>
+    <nav className="top-nav">
+      <img 
+        className="new-note-button" 
+        src={newNote} 
+        onClick={() => props.dispatch(addNote())} />
+      <input 
+        onChange={(e) => console.log(e.target.value)} 
+        type="text" 
+        placeholder="Search notes"/>
+      <label htmlFor="preview">Preview notes</label>
+      <input 
+        type="checkbox" 
+        id="preview"/>
+      <label htmlFor="recent">Sort by recent</label>
+      <input 
+        type="checkbox" 
+        id="recent"/>
+      <img 
+        className="save-note-button" 
+        src={hideList} 
+        onClick={() => props.dispatch(updateNoteInDb(props.currentDraft))} />
     </nav> 
   )
 }
