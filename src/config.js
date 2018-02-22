9f94919df83770578ecce261696e8780973a5dd1
Brent Guistwite <brentguistwite@gmail.com>
2018-02-22 17:57:29 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
 export default API_URL;
