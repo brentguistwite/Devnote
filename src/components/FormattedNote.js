c0ae5858e5baa9f9a9bc3fd1fda92ba1861cc5e1
Brent Guistwite <brentguistwite@gmail.com>
2018-02-24 22:55:26 -0500

    fix markdown library

--- src/components/FormattedNote.js
+++ src/components/FormattedNote.js
@@ -7,32 +7,31 @@ import Remarkable from 'remarkable';
 
 export function FormattedNote(props) {
   const md = new Remarkable('full', {
+    html: true,
     linkify: true,
     typographer: true,
     xhtmlOut: true,
-  },
-  {
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
           return hljs.highlight(lang, str).value;
-        } catch (err) { }
+        } catch (err) {
+          console.log('first block', err);
+        }
       }
 
       try {
         return hljs.highlightAuto(str).value;
-      } catch (err) { }
+      } catch (err) {
+        console.log('second block', err);
+      }
 
       return ''; // use external default escaping
     }
   });
-  const x = md.render(props.currentDraft.content);
-  console.log(x[0]);
-  x.replace(/\"/g, "");
-  console.log(x[0], typeof x);
-  // console.log(x.raw());
+  const html = md.render(props.currentDraft.content);
   return(
-    x
+    <div className="content" dangerouslySetInnerHTML={{ __html: html }}></div>
   );
 }
 
