0bda9bb5087a623f0634ce2edd4277eda7b4c94f
Brent Guistwite <brentguistwite@gmail.com>
2018-03-04 22:54:46 -0500

    add icon

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import {
   createStore,
   applyMiddleware,
-  // combineReducers,
 } from 'redux';
 import thunk from 'redux-thunk';
-// import { reducer as formReducer } from 'redux-form';
 import reducer from './reducers/reducer';
 
 export default createStore(
   reducer,
-  applyMiddleware(thunk),
+  applyMiddleware(thunk)
 );
